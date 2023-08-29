07fdf1b13e6d2a7980967ca5f2765145389cfdd4
Adriano Ribeiro <adrianotribeiro092@gmail.com>
2023-08-29 09:14:37 -0300

    feat/NIS-06 - implementação da blocagem da demonstração comercial

--- components/DemonstrationSection/index.tsx
+++ components/DemonstrationSection/index.tsx
@@ -48,8 +48,40 @@ export default function Demonstration(): React.ReactElement {
           </div>
         </div>
       </div>
-      <div className=" xl:w-full xl:bg-[#1D1B1BBF]/75">
-        <h1>Comercial</h1>
+      <div className="xl:flex xl:w-full xl:flex-col xl:items-center xl:gap-3 xl:bg-[#1D1B1BBF]/75 xl:py-8 5xl:gap-6">
+        <div className="xl:flex xl:w-full xl:flex-col xl:items-center">
+          <p className="xl:text-3xl xl:text-[#ffffff] 4xl:text-[35px] 5xl:text-[42px]">Comercial</p>
+          <hr className="xl:w-[30%] xl:border-b-[1px] xl:border-[#ffffff] 4xl:w-[35%] 5xl:w-[30%]" />
+        </div>
+        <p className="xl:w-[100%] xl:px-8 xl:text-justify xl:text-2xl xl:font-normal xl:text-[#ffffff] 4xl:px-12 5xl:px-24 5xl:text-3xl">
+          Mauris vitae risus urna. Phasellus placerat sit amet nulla non lobortis. Pellentesque sed interdum enim.
+          Aliquam porttitor sem sed turpis blandit sagittis. Vestibulum ornare, elit non efficitur placerat.
+        </p>
+        <div className="xl:flex xl:w-full xl:gap-2 xl:px-6 4xl:px-6 4_xl:px-0">
+          <div className="xl:flex xl:flex-wrap xl:justify-center xl:gap-3">
+            <Image
+              width={297}
+              height={167}
+              src="/images/planta_baixa_comercial.png"
+              alt=""
+              className="xl:h-fit xl:w-[278px] 3xl:h-[200px] 3xl:w-[300px] 4xl:w-[300px] 4_xl:h-[200px] 4_xl:w-[325px] 5xl:h-[200px] 5xl:w-[375px] "
+            />
+            <Image
+              width={297}
+              height={167}
+              src="/images/planta_baixa_comercial.png"
+              alt=""
+              className="xl:h-fit xl:w-[278px] 3xl:h-[200px] 3xl:w-[300px] 4xl:w-[300px] 4_xl:h-[200px] 4_xl:w-[325px] 5xl:h-[200px] 5xl:w-[375px] "
+            />
+            <Image
+              width={297}
+              height={167}
+              src="/images/planta_baixa_comercial_01.png"
+              alt=""
+              className="xl:h-fit xl:w-[568px] 3xl:h-[260px] 3xl:w-[610px] 4xl:h-[230px] 4xl:w-[612px] 4_xl:h-[260px] 4_xl:w-[660px] 5xl:h-[270px] 5xl:w-[760px]"
+            />
+          </div>
+        </div>
       </div>
     </section>
   )
