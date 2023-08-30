c07a7063b481dbe5f7ca2a0dc49b52bd7db5af47
lnormanha <luiznormanha@gmail.com>
2023-08-30 11:00:47 -0300

    feat: added responsiveness

--- pages/projetos/index.tsx
+++ pages/projetos/index.tsx
@@ -8,31 +8,29 @@ export default function Projects(): React.ReactElement {
 
   return (
     <div className="flex flex-col items-center py-4">
-      <div className="mb-24 mt-8 flex w-[40%] flex-row items-center justify-center gap-4 border-b border-[#676060] p-4">
+      <div className="mb-24 mt-8 flex flex-row items-center justify-center gap-4 border-b border-[#676060] p-4 xl:w-[40%]">
         <button onClick={() => setProjectsType("RESIDENCIAL")}>
           <p
-            className={`font-montserrat font-light tracking-wider xl:text-3xl xl:text-[${textColor(
+            className={`font-montserrat text-3xl font-light tracking-wider xs:text-xl  xl:text-[${textColor(
               "RESIDENCIAL"
-            )}] duration-100 ease-in 4xl:text-[32px] 5xl:text-[32px]`}
+            )}] duration-100 ease-in `}
           >
             RESIDENCIAL
           </p>
         </button>
 
-        <p className="font-montserrat font-light tracking-wider xl:text-3xl xl:text-[#676060] 4xl:text-[42] 5xl:text-[42px]">
-          /
-        </p>
+        <p className="font-montserrat text-3xl font-light tracking-wider xl:text-[#676060] xs:text-xl ">/</p>
         <button onClick={() => setProjectsType("COMERCIAL")}>
           <p
-            className={`font-montserrat font-light tracking-wider xl:text-3xl xl:text-[${textColor(
+            className={`font-montserrat text-3xl  font-light  tracking-wider xs:text-xl xl:text-[${textColor(
               "COMERCIAL"
-            )}] duration-100 ease-in 4xl:text-[32px] 5xl:text-[32px]`}
+            )}] duration-100 ease-in`}
           >
             COMERCIAL
           </p>
         </button>
       </div>
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid items-center gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  xs:grid-cols-1">
         {ProjectsData.map((project) => (
           <ProjectCard project={project} key={project.id} />
         ))}
