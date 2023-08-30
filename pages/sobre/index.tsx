3c9b82be4b1d4b4066048b824e924652761586e0
lnormanha <luiznormanha@gmail.com>
2023-08-30 14:49:52 -0300

    feat: responsiveness

--- pages/sobre/index.tsx
+++ pages/sobre/index.tsx
@@ -5,19 +5,23 @@ export default function Sobre(): React.ReactElement {
     <div className="flex h-full w-full flex-col items-center">
       <div className="flex h-full w-full flex-col items-center bg-[url('/images/bg_00.png')]">
         <div className="flex h-full w-full flex-col items-center justify-center bg-[#D9D9D9]/80 ">
-          <div className="grid h-full w-full grid-cols-5">
-            <div className="col-span-3 flex h-full w-full flex-col items-center justify-center">
-              <div className="m-4 border-b border-[#676060] p-4">
-                <h1 className="font-montserrat text-7xl font-light tracking-wider">Eduardo Hoerlle</h1>
+          <div className="grid h-full w-full grid-cols-2 md:grid-cols-5">
+            <div className="col-span-2 flex h-1/6 w-full flex-col items-center justify-center md:col-span-3 md:h-full">
+              <div className="m-2 border-b border-[#676060] p-2 md:m-4 md:p-4">
+                <h1 className="mt-12 font-montserrat text-4xl font-light tracking-wider md:mt-0 md:text-7xl">
+                  Eduardo Hoerlle
+                </h1>
               </div>
-              <h2 className="mb-16 text-2xl font-light tracking-wider">Lorem ipsum dolor sit amet.</h2>
-              <p className="w-8/12 text-xl font-light tracking-wider">
+              <h2 className="mb-8 text-lg font-light tracking-wider md:mb-16 md:text-2xl">
+                Lorem ipsum dolor sit amet.
+              </h2>
+              <p className="w-10/12 text-base font-light tracking-wider md:w-8/12 md:text-xl">
                 In malesuada enim id aliquam rutrum. Etiam vitae nunc vehicula, volutpat leo et, viverra elit. Curabitur
                 leo orci, feugiat ac facilisis ut, pellentesque ac massa. Phasellus quis vulputate felis. Praesent
                 maximus quam sagittis, pretium augue a, pulvinar justo.
               </p>
             </div>
-            <div className="col-span-2 h-full">
+            <div className="order-first col-span-2 h-5/6 md:order-last md:col-span-2 md:h-full ">
               <Image
                 width={500}
                 height={300}
