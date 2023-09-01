4ca03ce339c3f9625a6eb654aa053041f2b53b29
Adriano Ribeiro <adrianotribeiro092@gmail.com>
2023-08-31 22:46:36 -0300

    feat/NIS-09 - implementação do footer

---
+++ components/FooterSection/index.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Footer(): React.ReactElement {
+  return (
+    <section className="xl:flex xl:justify-around xl:py-2 6xl:m-auto 6xl:w-[70%]">
+      <div>
+        <p className="font-montserrat md:text-[35px] md:text-[#676060] xl:text-[40px]">Eduardo Hoerlle</p>
+      </div>
+      <div className="xl:flex xl:items-center xl:gap-2 xl:text-[#676060]">
+        <p>©</p>
+        <p> 2023 Eduardo Hoerlle - Desenvolvido por Nissian Software</p>
+      </div>
+    </section>
+  )
+}
